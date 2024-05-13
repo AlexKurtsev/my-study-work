e4e83bb858d3dcfec8a90797ea3639189aafa82f
alexey_kurtsev <alexeykurtsev@yandex.ru>
2024-05-14 11:08:53 +1200

    Add_solution_task_5

---
+++ Module17/06_two_dimensional_list/main.py
@@ -0,0 +1,5 @@
+print([list(range(0 + i_num, 9 + i_num, 4)) for i_num in range(1, 5)])
+
+###############################################################################
+
+print([[(i_num + j_num) for i_num in range(0, 10, 4)] for j_num in range(1, 5)])
